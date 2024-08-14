7d19164e39075a7fb3bebefa0aae22469d7dd890
GitSentinel <mahanta.jacinth2021@gmail.com>
2024-08-15 02:14:56 +0530

    [UPD]: Rag-Chat library added!

---
+++ src/app/[...url]/page.tsx
@@ -0,0 +1,12 @@
+interface PageProps {
+  params: {
+    url: string | string[] | undefined;
+  };
+}
+
+const Page = ({ params }: PageProps) => {
+  console.log(params);
+  return <div>Page</div>;
+};
+
+export default Page;
